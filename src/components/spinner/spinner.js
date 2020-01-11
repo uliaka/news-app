49fef040a5fea3af2e5316dd3280e9a920c9011f
uliaka <uliana.gorin95@gmail.com>
2020-01-11 17:14:45 +0200

    created spinner component

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+
+const SpinnerContainer = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`
+const SpinnerSection = styled.div`
+  border: 6px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 6px solid grey;
+  width: 30px;
+  height: 30px;
+  animation: ${spin} 1.5s linear infinite;
+`
+
+
+const Spinner = () => (
+  <SpinnerContainer>
+    <SpinnerSection/>
+  </SpinnerContainer>
+)
+
+export default Spinner;
--- src/pages/newsListPage.js
+++ src/pages/newsListPage.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import NewsList from '../components/news/newsList'
 import useFetchData from './useFetchData';
+import Spinner from '../components/spinner/spinner';
 
 const NewsListPage = () => {
 const [result, loading, error] = useFetchData();
 const items = (result && result.articles) || [];
   return (
+    <>
+    <Spinner/>
     <NewsList items={items} />
+    </>
   );
 }
 
